0a2bd5394625a1b905ef468df4c5fd5d104e49a1
Cynthia Johnsen <vaj.cynthia@gmail.com>
2022-02-01 10:21:50 -0600

    3rd commit

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # BlogAppAPI
+
+Installation Instructions:
+
+1.  Download or git clone the repository
+2.  use npm -i to install the necessary packages
+3.  Create a MongoDB Atlas account. Replace MONGODB_URI environmental variable with your own connection string.
+4.  Must register a user first. Then, login and the authorization token will record in the response header. Also, include the authorization token in the request header when using the various ../blog/.. endpoints.
+
+Environmental variables
+Port
+MONGODB_URI
+JWT_SECRET
+SALT
+
+DOCUMENTATION:
+/
+-Get
+Returns String "API UP"
+
+/auth
+/login
+-username: string, required
+-password: string, required
+Returns:
+Full user objects (no password)
+Header: Authorization (jwt)
+
+/register - BODY - username: - password: - firstname: - lastname: - email:  
+Returns:
+User Body
+
+/auth
+/:login
+/:register
+
+/blogs
+/:username
+/:id
--- routes/blogsRoute.js
+++ routes/blogsRoute.js
@@ -1,20 +1,82 @@
 const express = require('express')
+const Blog = require('../schema/blogSchema')
 const blogsRoute = express.Router()
+const verifyJWT = require("../middleware/jwt")
 
 
-blogsRoute.post('/username', (req, res)=>{
+blogRouter.get("/", (req, res) => {
+Blog.find((error, result) => {
+    if (error) {
+    res.status(404).json({error: error.message});
+}
+    else {res.status(200).json({message: result.filter((entry) => {
+    return entry.private === false;
+            })
+        })
+        }
+    })
+})
+
+blogsRoute.post('/:username',verifyJWT, (req, res)=>{
    let username =req.params.username
-   res.status(200).json({message: err.message})
+   let blog= req.body
+   let createdBy = blog.created_by
+    if (createdBy !== username) {
+        res.status(403).json({message: "Username is incorrect"
+        })
+    }
+    else {
+        Blog.create(blog, (error, result) => {
+            if (error) {
+                res.status(400).json({message: error.message });
+            }
+            else {
+                res.status(200).json({data: result });
+            }
+        })
+    }
+})
+blogRouter.get("/:username", verifyJWT, (req, res) => {
+    const username = req.params.username;
+    Blog.find({ username: username }, (error, result) => {
+        if (error) {
+            res.status(400).json({ message: error.message });
+        }
+        else {
+            res.status(200).json({
+                message: result
+            })
+        }
+    })
 })
 
-
-blogsRoute.post('/id', (req, res)=>{
-    let id= req.params.id 
-    res.status(200).json({message: err.message})
+blogRouter.put("/:id", verifyJWT, (req, res) => {
+    const id = req.params.id;
+    const blog = req.body;
+    let newBlog;
+    Blog.findOneAndUpdate({ _id: id }, req.body, (error, result) => {
+        if (error) {
+            res.status(400).json({ message: error.message });
+        }
+        else {
+            res.status(200).json({
+                message: result
+            })
+        }
+    })
 })
-
-
-
+blogRouter.delete("/:id", verifyJWT, (req, res) => {
+    const id = req.params.id;
+    const blog = req.body;
+    Blog.deleteOne({ _id: id }, (error) => {
+        if (error) {
+            res.status(400).json({ message: error.message });
+        }
+    })
+    res.status(200).json({
+        message: "Deleted Successfully!"
+    })
+})
 
 
 
